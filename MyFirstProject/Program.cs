ea9c3f09789b1100affd38fedd7f6d74e04cbc48
DESKTOP-9VIJQJV\Panteleimon <msa16101984@gmail.com>
2020-12-06 14:02:56 +0200

    Signed-off-by: DESKTOP-9VIJQJV\Panteleimon <msa16101984@gmail.com>

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int some = 7;
+            some = 6;
+            Console.WriteLine(some);
+            int package = 5;
+            Console.WriteLine(package); 
+
+        }
+    }
+}
